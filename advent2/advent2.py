53e9c206eb3fe7a4a097ad565b31b9994c825037
unknown <craig246@gmail.com>
2018-12-04 18:35:10 -0500

    successful q2

--- advent2/advent2.py
+++ advent2/advent2.py
@@ -1,5 +1,3 @@
-import re
-
 count2 = 0
 count3 = 0
 
@@ -7,17 +5,15 @@ ledger = []
 with open('input.txt', 'r') as f:
     for line in f.readlines():
         ledger.append(line.strip('\n'))
-   
+
 for barcode in ledger:
+    dicto = {}
     for letter in barcode:
-        if len(re.findall(letter, barcode)) == 3:
-            count3 += 1
-            barcode = re.sub(letter,'',barcode)
-            break
-        if len(re.findall(letter, barcode)) == 2:
-            count2 += 1
-            barcode = re.sub(letter,'',barcode)
-            
+        if letter not in dicto:
+            dicto[letter] = barcode.count(letter)
+    if 2 in dicto.values():
+        count2 += 1
+    if 3 in dicto.values():
+        count3 += 1
+
 print(count2*count3)
-
-
