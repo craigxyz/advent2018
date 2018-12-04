798f13ab9c3047a980980947ee76a94fdd8bde99
unknown <craig246@gmail.com>
2018-12-04 17:50:22 -0500

    some changes

--- advent2/advent2.py
+++ advent2/advent2.py
@@ -1,19 +1,22 @@
+import re
 ledger = []
 count2 = 0
 count3 = 0
-import re
 
 with open('input.txt', 'r') as f:
     for line in f.readlines():
         ledger.append(line.strip('\n'))
-        
+   
 for barcode in ledger:
     for letter in barcode:
         if len(re.findall(letter, barcode)) == 3:
             count3 += 1
             barcode = re.sub(letter,'',barcode)
-
-        
-        
+            break
+        if len(re.findall(letter, barcode)) == 2:
+            count2 += 1
+            barcode = re.sub(letter,'',barcode)
+            
+print(count2*count3)
 
 
