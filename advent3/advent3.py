67eff471fbb0d5d2efea8fbfd53880b0fb321562
unknown <craig246@gmail.com>
2018-12-07 09:53:51 -0500

    day 3 done

--- advent3/advent3.py
+++ advent3/advent3.py
@@ -1,16 +1,27 @@
 import numpy as np
 
+claims = []
 fabric = np.zeros((1000,1000))
 
-for line in open('input.txt'):
-    parts = line.split(' ')
-    left, top = map(int, parts[2][:-1].split(","))
-    width, height = map(int, parts[3].split('x'))
-    fabric[left:left+width, top:top+height] += 1
+def parse_claims():
+    for line in open('input.txt'):
+        parts = line.split(' ')
+        id = parts[0]
+        x, y = map(int, parts[2][:-1].split(","))
+        dx, dy = map(int, parts[3].split('x'))
+        claims.append([id,x,y,dx,dy])
 
-#part 1
-#print(np.sum(fabric > 1))
+def p1():
+    for (id,x,y,dx,dy) in claims:
+        fabric[x:x+dx, y:y+dy] += 1
+    return(np.sum(fabric > 1))
 
+def p2():
+    for (id,x,y,dx,dy) in claims:
+        if np.all(fabric[x:x+dx, y:y+dy] == 1):
+            return id
 
-
+parse_claims()
+print(p1())
+print(p2())
 
