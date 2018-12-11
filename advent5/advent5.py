4283b5ab5381de6fe780cfb35102768c0320c50a
unknown <craig246@gmail.com>
2018-12-11 10:05:46 -0500

    Cleaned up p1

--- advent5/advent5.py
+++ advent5/advent5.py
@@ -1,12 +1,14 @@
 data = open('input.txt').read().strip()
-length = len(data)
-oldLength = length + 1 # See comment below
 
-while length != oldLength: # If length is unchanged after a pass, we are done. 
-    oldLength = length # Now the old length is set
-    for each in 'abcdefghijklmnopqrstuvwxyz': # do our replacements
-        data = data.replace(each+each.upper(),"") 
-        data = data.replace(each.upper()+each,"")
-        length = len(data) # Length is set after replacements
-print(length)
-    
+def react(polymer):
+    length = len(polymer)
+    oldLength = None
+    while length != oldLength: # If length is unchanged after a pass, we are done. 
+        oldLength = length # Now the old length is set
+        for each in 'abcdefghijklmnopqrstuvwxyz': # do our replacements
+            polymer = polymer.replace(each+each.upper(),"") 
+            polymer = polymer.replace(each.upper()+each,"")
+            length = len(polymer) # Length is set after replacements
+    print(length)
+
+print(react(data))
